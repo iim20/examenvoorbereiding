c6c7f73503ef973ac32d528d6361ae19ff3fe49a
Black Hole Recordings <maxa_med_2@hotmail.com>
2023-03-02 15:10:01 +0100

    Gefixt active tab in create

--- snoepfabriekvanmelle/resources/views/partials/nav.blade.php
+++ snoepfabriekvanmelle/resources/views/partials/nav.blade.php
@@ -6,7 +6,7 @@
     </a>
   </div>
   <div class="flex items-baseline gap-x-12 pr-1">
-    <a href="storingen" class="{{ request()->is('storingen') ? 'active' : '' }} text-lg font-medium leading-6 text-gray-900">Storingen</a>
+    <a href="{{ route('storingen.index') }}" class="{{ request()->is('storingen*', 'create') ? 'active' : '' }} text-lg font-medium leading-6 text-gray-900">Storingen</a>
     <a href="medewerkers" class="{{ request()->is('medewerkers') ? 'active' : '' }} text-lg font-medium leading-6 text-gray-900">Medewerkers</a>
     <a href="#" class="text-lg font-medium leading-6 text-gray-900">IT contact</a>
   </div>
