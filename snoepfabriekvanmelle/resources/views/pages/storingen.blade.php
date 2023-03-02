821a40db59b72afa4da386ed2daf0de388dc0318
Black Hole Recordings <maxa_med_2@hotmail.com>
2023-03-02 15:11:32 +0100

    Storingen test blade verwijderd

--- snoepfabriekvanmelle/resources/views/pages/storingen.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-    <div class="flex items-center justify-center min-h-screen">
-        <h1 class="text-blue-600 text-4xl">Content</h1>
-    </div>
-@endsection
