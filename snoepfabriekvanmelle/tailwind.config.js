f80484f97634253b30476dff643ce07c93c7439f
Black Hole Recordings <maxa_med_2@hotmail.com>
2023-03-04 20:44:08 +0100

    Laravel breeze auth

--- snoepfabriekvanmelle/tailwind.config.js
+++ snoepfabriekvanmelle/tailwind.config.js
@@ -1,21 +1,25 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./resources/**/*.blade.php",
-    "./resources/**/*.js",
-    "./resources/**/*.vue",
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        'poppins': ['Poppins', 'sans-serif'],
-      },
-      colors: {
-        'white': '#ffffff',
-        'blue': '#0040C0',
-        'black': '#161616',
-      }
+    content: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+    ],
+
+    theme: {
+        extend: {
+          fontFamily: {
+            'poppins': ['Poppins', 'sans-serif'],
+          },
+          colors: {
+            'white': '#ffffff',
+            'blue': '#0040C0',
+            'black': '#161616',
+          }
+        }
     },
-  },
-  plugins: [],
-}
+
+    plugins: [require('@tailwindcss/forms')],
+};
