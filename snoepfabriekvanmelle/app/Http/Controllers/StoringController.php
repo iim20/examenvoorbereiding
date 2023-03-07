84944baa3b76d222585e77add373b213de7a10c0
Black Hole Recordings <maxa_med_2@hotmail.com>
2023-03-07 12:07:48 +0100

    Validation form

--- snoepfabriekvanmelle/app/Http/Controllers/StoringController.php
+++ snoepfabriekvanmelle/app/Http/Controllers/StoringController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreFormRequest;
 use App\Models\Storing;
 use Attribute;
 use Illuminate\Http\Request;
@@ -29,7 +30,7 @@ class StoringController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreFormRequest $request)
     {
         $description = $request->input('description');
         $machineId = $request->input('machine_id');
